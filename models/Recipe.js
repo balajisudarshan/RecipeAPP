d7b01e4511e07742cb68873611e8dd14593c3dcf
Kondreddy Balaji Sudarshan Reddy <kondreddy2007@gmail.com>
2025-09-06 09:54:35 +0530

    added image

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,59 +1,71 @@
-const mongoose =require('mongoose')
+const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema({
-  title:{
-    type:String,
-    required:true,
+  title: {
+    type: String,
+    required: true,
   },
-  description:{
-    type:String,
-    required:true,
+  description: {
+    type: String,
+    required: true,
   },
-  ingredients:{
-    type:[String],
-    required:true,
+  ingredients: {
+    type: [String],
+    required: true,
   },
-  
-  image:{
-    type:String,
-    default:"https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/quick_flatbreads_43123_16x9.jpg"
+
+  image: {
+    type: String,
+    default: "", // leave blank, we’ll set conditionally in pre-save
   },
-  likes:[{
-    type:mongoose.Schema.ObjectId,
-    ref:'User'
-  }],
-  createdBy:{
-    type:mongoose.Schema.ObjectId,
-    ref:'User',
-    required:true
+  likes: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+    },
+  ],
+  createdBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true,
   },
-  creatorName:{
-    type:String,
-    // ref:'User',
-    // required:true
+  creatorName: {
+    type: String,
   },
   foodType: {
     type: String,
-    enum: ["veg", "non-veg"], // only allow these values
+    enum: ["veg", "non-veg"],
     required: true,
   },
-})
+});
 
-
+// Pre-save hook for defaults
 recipeSchema.pre("save", async function (next) {
   try {
+    // Assign creator name
     if (!this.creatorName && this.createdBy) {
       const User = mongoose.model("User");
       const user = await User.findById(this.createdBy).select("username");
-
       if (user) {
         this.creatorName = user.username;
       }
     }
-    next(); 
+
+    // Assign image if not provided
+    if (!this.image || this.image.trim() === "") {
+      if (this.foodType === "veg") {
+        this.image =
+          "https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=800&q=80"; // veg default
+      } else {
+        this.image =
+          "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?auto=format&fit=crop&w=800&q=80"; // non-veg default
+      }
+    }
+
+    next();
   } catch (error) {
-    next(error); 
+    next(error);
   }
 });
 
-module.exports = mongoose.model('Recipe',recipeSchema)
+module.exports = mongoose.model("Recipe", recipeSchema);
